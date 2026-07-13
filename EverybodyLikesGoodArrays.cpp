20591031f05a34472a96b28209640ee22813dcbe
himanshu6093 <himanshu.padhai@gmail.com>
2026-07-13 23:50:20 +0530

    13 july easy day

---
+++ EverybodyLikesGoodArrays.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int count =0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        if ((a[i] % 2 == 0 && a[i + 1] % 2 == 0) || (a[i] % 2 != 0 && a[i + 1] % 2 != 0))
+        {
+
+            count++;
+        }
+    }
+    cout << count << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
